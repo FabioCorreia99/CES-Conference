d0910f75a6853ef3aa4038b3ca70a5818972d94d
diana-goncalves <dianafi96@gmail.com>
2024-11-14 22:18:27 +0000

    Create the routes (edit version)

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,63 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
+import AboutView from "../views/AboutView.vue";
+import ScheduleView from "../views/ScheduleView.vue";
+import SpeakersView from "../views/SpeakersView.vue";
+import PartnersView from "../views/PartnersView.vue";
+import BuyTicketView from "../views/BuyTicketView.vue";
+import LoginView from "../views/LoginView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
+      path: "/",
+      name: "home",
       component: HomeView,
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
+      path: "/schedule",
+      name: "schedule",
+      component: ScheduleView,
+    },
+    {
+      path: "/speakers",
+      name: "speakers",
+      component: SpeakersView,
+    },
+    {
+      path: "/partners",
+      name: "partners",
+      component: PartnersView,
+    },
+    {
+      path: "/buy-ticket",
+      name: "buyTicket",
+      component: BuyTicketView,
+      meta: { requiresAuth: true }, // login para comprar bilhetes
+    },
+    {
+      path: "/login",
+      name: "login",
+      component: LoginView,
+    },
+    {
+      path: "/about",
+      name: "about",
+      component: AboutView,
     },
   ],
-})
+});
 
-export default router
+// router.beforeEach((to, from, next) => {
+//   // const isAuthenticated = localStorage.getItem("isAuthenticated");
+
+//   if (to.meta.requiresAuth && !isAuthenticated) {
+//     // next("/login")
+//     next({ path: "/login", query: { from: to.path } });
+//   } else {
+//     next();
+//   }
+// });
+
+export default router;
