acddd6e6712405dc7c9cb5ec6cd1a8e1fba00207
andresjpg2000 <andresjpg2000@gmail.com>
2024-12-21 19:13:01 +0000

    NavBar implementada

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,20 +2,20 @@
 
     <nav id="navbar">
 
-        <RouterLink :to="{ name: 'home' }">
+        <RouterLink :to="{ name: 'home' }" id="LogoLink" :class="{active: isCurrentRoute('home')}">
             <img src="../assets/media/Logo.svg" alt="CES logo" class="nav-logo" />
         </RouterLink>
         <div class="links">
-            <RouterLink :to="{ name: 'schedule' }">Schedule</RouterLink>
-            <RouterLink :to="{ name: 'speakers' }">Speakers</RouterLink>
-            <RouterLink :to="{ name: 'forum' }">Forum</RouterLink>
-            <RouterLink :to="{ name: 'partners' }">Partners</RouterLink>
-            <RouterLink :to="{ name: 'about' }">About</RouterLink>
-            <RouterLink :to="{ name: 'login' }">Sign Up</RouterLink>
+            <RouterLink :to="{ name: 'schedule'}" :class="{active: isCurrentRoute('schedule')}">Schedule</RouterLink>
+            <RouterLink :to="{ name: 'speakers'}" :class="{active: isCurrentRoute('speakers')}">Speakers</RouterLink>
+            <RouterLink :to="{ name: 'forum'}" :class="{active: isCurrentRoute('forum')}">Forum</RouterLink>
+            <RouterLink :to="{ name: 'partners'}" :class="{active: isCurrentRoute('partners')}">Partners</RouterLink>
+            <RouterLink :to="{ name: 'about'}" :class="{active: isCurrentRoute('about')}">About</RouterLink>
+            <RouterLink :to="{ name: 'login'}" id="LoginLink" :class="{active: isCurrentRoute('login')}">Sign Up</RouterLink>
         </div>
 
-        <div id="menu">
-            <Bars3Icon class="text-white" />
+        <div id="menu" @click="toggleNAV()" @mouseover="hoverColor = '#F2A714'" @mouseleave="hoverColor = '#ffff'">
+            <Bars3Icon :style="{color: hoverColor}" />
         </div>
 
     </nav>
@@ -23,17 +23,47 @@
 </template>
 
 <script>
-import { RouterLink } from 'vue-router'
+import { RouterLink, useRoute } from 'vue-router'
 import { Bars3Icon } from '@heroicons/vue/24/solid'
 
 export default {
     components: {
         Bars3Icon,
     },
+    data() {
+        return {
+            isNavOpen: JSON.parse(sessionStorage.getItem("isNavOpen")) || false,
+            hoverColor: "#ffff",
+        }
+    },
+    setup() {
+        const route = useRoute();
+        const isCurrentRoute = (name) => route.name === name;
+
+        return {isCurrentRoute};
+    },
+    methods: {
+        toggleNAV() {
+            this.isNavOpen = !this.isNavOpen;
+            sessionStorage.setItem("isNavOpen", JSON.stringify(this.isNavOpen));
+            
+            const links = document.querySelector(".links");
+            
+            if (this.isNavOpen) {
+                links.style.display = "flex";
+            } else {
+                links.style.display = "none";
+            }
+            
+        }
+    },
+    mounted() {
+        document.querySelector(".links").style.display = this.isNavOpen ? "flex" : "none";
+    }
 }
 </script>
 
-<style>
+<style scoped>
 #navbar {
     display: flex;
     justify-content: center;
@@ -48,6 +78,7 @@ export default {
 }
 
 .nav-logo {
+    display: flex;
     height: 30px;
     width: 121px;
     align-self: center;
@@ -59,5 +90,60 @@ export default {
     justify-content: center;
     width: 34px;
     height: 30px;
+    cursor: pointer;
 }
+
+#LogoLink {
+    width: 137px;
+}
+
+#LoginLink {
+    background-color: rgba(255,255,255, 0.3);
+    border-radius: 10px;
+}
+
+.links {
+    display: flex;
+    gap: 16px;
+}
+
+a {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 112px;
+    text-align: center;
+    position: relative;
+}
+
+a::after {
+    content: "_";
+    opacity: 0;
+    transition: opacity 0.25 ease;
+}
+
+a:hover::after,
+a.active::after {
+    /* background-color: hsla(160, 100%, 37%, 0.2); */
+    animation: pulse 1s infinite;
+}
+
+@keyframes pulse {
+    0% {
+        opacity: 1;
+    }
+    50% {
+        opacity: 0;
+    }
+    100% {
+        opacity: 1;
+    }
+}
+
+.links RouterLink {
+    display: inline-block;
+    width: 97px;
+    text-align: center;
+}
+
 </style>
