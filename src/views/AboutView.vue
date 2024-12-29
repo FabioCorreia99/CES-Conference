fb9b9986474bbe9e794c9d17ac1670f6c0d72d66
diana-goncalves <dianafi96@gmail.com>
2024-12-29 17:48:18 +0000

    Add introTitle to AboutView

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,14 +1,30 @@
 <template>
+  <v-app>
+    <v-row>
+      <!-- Navbar Column -->
+      <v-col>
+        <header>
+          <Navbar />
+        </header>
+      </v-col>
+    </v-row>
 
-  <header>
-
-    <Navbar />
-
-  </header>
-
-  <div>
-    About
-  </div>
+    <v-main>
+      <v-container>
+        <v-row class="test">
+          <v-col>
+            <h1 class="introTitle my-12">About</h1>
+            <h6 class="textTitle my-12 w-75">CES® is the world's most impactful tech event — the launchpad for
+              groundbreaking ideas and visionary creators.</h6>
+            <div class="d-flex justify-space-around my-12 w-50">
+            </div>
+          </v-col>
+        </v-row>
+        <v-row class="aboutContainer">
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
@@ -22,4 +38,31 @@ export default {
 
 </script>
 
-<style scoped></style>
+<style scoped>
+.bg-intro {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 28rem;
+  margin: 0;
+  background-color: rgba(189, 199, 211, 0.30);
+}
+
+.introTitle {
+  color: #26466D;
+  font-family: "Source Code Pro";
+  font-size: 6.25rem;
+  font-style: normal;
+  font-weight: 300;
+  line-height: normal;
+}
+
+.textTitle {
+  color: #26466D;
+  font-size: 1.125rem;
+  font-style: normal;
+  font-weight: 300;
+  line-height: normal;
+}
+</style>
