037088b7960e2dd03aa7e570e9e332eaea3e37a7
M4sterSoul <fabio1999c@gmail.com>
2025-01-03 00:11:32 +0000

    implement of TalksCard(not finished)

---
+++ src/components/TalksCard.vue
@@ -0,0 +1,26 @@
+<template>
+    <v-card
+        class="mx-1 titleCard"
+        rounded="lg"
+        width="24.8125rem"
+        color='var(--color-dark-blue)'
+        title="Avatars"
+        subtitle="prepend-avatar and append-avatar"
+        append-avatar="https://cdn.vuetifyjs.com/images/john.jpg"
+    >
+        <v-divider class="mx-4 mt-0" thickness="3" color="var(--color-light-blue)"></v-divider>
+        <v-card-text>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod.</v-card-text>
+    </v-card>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style scoped>
+.titleCard{
+    color: var(--color-white);
+}
+</style>
--- src/views/ScheduleView.vue
+++ src/views/ScheduleView.vue
@@ -1,32 +1,37 @@
 <template>
 <v-app>
-    <header class="position-absolute">
-        <Navbar />
-    </header>
+  <header class="position-absolute">
+    <Navbar />
+  </header>
 
-    <v-main>
-        <v-row class="bg-intro mx-0 pt-10 w-100">
-            <v-col>
-                <h1 class="introTitle my-12">Schedule</h1>
-                <h6 class="textTitle my-12 w-75">Explore your options to connect, learn and be inspired at CES.</h6>
-                <div class="d-flex justify-space-around my-1 w-50">
-
-                </div>
-            </v-col>
-        </v-row>
-      
-        <v-row class="mt-16">
-          <v-expansion-panels class="w-100" multiple  bg-color="rgba(189, 199, 211, 0.3)" elevation="0" static collapse-icon="mdi-chevron-up" expand-icon="mdi-chevron-down">
-            <v-expansion-panel v-for="hour in hours" class="scheduleMenu">
-              <v-expansion-panel-title class="text-h6">{{ hour }}</v-expansion-panel-title>
-              <v-expansion-panel-text>
-
-              </v-expansion-panel-text>
-            </v-expansion-panel>
-          </v-expansion-panels>
-        </v-row>
-    </v-main>
-  </v-app>
+  <v-main>
+    <v-row class="bg-intro mx-0 pt-10 w-100 mb-8">
+      <v-col>
+        <h1 class="introTitle my-12">Schedule</h1>
+        <h6 class="textTitle my-12 w-75">Explore your options to connect, learn and be inspired at CES.</h6>
+        <div class="d-flex justify-space-around my-1 w-50"></div>
+      </v-col>
+    </v-row>
+            
+    <v-row class="mt-16">
+      <v-expansion-panels class="w-100" multiple  bg-color="rgba(189, 199, 211, 0.3)" elevation="0" collapse-icon="mdi-chevron-up" expand-icon="mdi-chevron-down">
+        <v-expansion-panel v-for="hour in hours" class="scheduleMenu mb-4">
+          <v-expansion-panel-title class="text-h6">{{ hour }}</v-expansion-panel-title>
+          <v-expansion-panel-text>
+            <v-row>
+              <v-col v-for="n in 5" lg="4" md="6" sm="12" class="d-flex justify-center">
+                <TalksCard/>
+              </v-col>
+            </v-row>
+          </v-expansion-panel-text>
+        </v-expansion-panel>
+      </v-expansion-panels>
+    </v-row>
+  </v-main>
+<!-- 
+   -->
+  <h6>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit ipsam harum dolor aliquam at incidunt laborum enim officiis magnam commodi soluta voluptatum, aspernatur velit ducimus necessitatibus voluptas aperiam facere fuga?</h6>
+</v-app>
 
 <link href="https://cdn.jsdelivr.net/npm/@mdi/font/css/materialdesignicons.min.css" rel="stylesheet">
 </template>
@@ -34,13 +39,15 @@
 <script>
 import Navbar from '@/components/Navbar.vue';
 import SpeakersCard from '@/components/SpeakersCard.vue';
+import TalksCard from '@/components/TalksCard.vue';
 
 
 export default {
     name: "ScheduleView",
     components: {
         Navbar,
-        SpeakersCard
+        SpeakersCard,
+        TalksCard
     },
     data() {
       return {
@@ -63,7 +70,6 @@ export default {
 <style >
 .scheduleMenu{
   color: var(--color-dark-blue) !important;
-  height: 3.5rem;
   padding: 0 6.5rem !important;
 }
 .v-expansion-panel .v-icon {
