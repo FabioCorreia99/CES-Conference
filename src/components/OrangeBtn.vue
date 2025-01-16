22b0e12816e74587b0d1c5dedbd2e1b462296d5e
diana-goncalves <dianafi96@gmail.com>
2025-01-16 16:50:51 +0000

    add: emits: ["click"]

--- src/components/OrangeBtn.vue
+++ src/components/OrangeBtn.vue
@@ -1,42 +1,43 @@
 <template>
-    <v-hover v-slot="{ isHovering, props }">
-        <v-btn 
-            :to="{ name: route }"
-            elevation="4" 
-            size="large" 
-            :color="isHovering ?  '#EEF6F2' : '#F2A714'"
-            class="rounded-lg Orangebtn"
-            v-bind="props"
-            ><span 
-                class="text-lowercase text-button"
-                :class="{ 'on-hover': isHovering }"
-                v-bind="props"
-                >/{{ value }}
-            </span>
-        </v-btn>
-    </v-hover>
+  <v-hover v-slot="{ isHovering, props }">
+    <v-btn
+      :to="{ name: route }"
+      elevation="4"
+      size="large"
+      :color="isHovering ? '#EEF6F2' : '#F2A714'"
+      class="rounded-lg Orangebtn"
+      v-bind="props"
+      ><span
+        class="text-lowercase text-button"
+        :class="{ 'on-hover': isHovering }"
+        v-bind="props"
+        >/{{ value }}
+      </span>
+    </v-btn>
+  </v-hover>
 </template>
 
 <script>
-    export default {
-    props: {
-        value: {
-            type: String,
-            required: true,
-        },
-        route: {
-            type: String,
-            default: "home"
-        },
-     },   
-    }
+export default {
+  props: {
+    value: {
+      type: String,
+      required: true,
+    },
+    route: {
+      type: String,
+      default: "home",
+    },
+  },
+  emits: ["click"], // Declara que o componente emite o evento "click"
+};
 </script>
 
 <style scoped>
-span{
-    color: var(--color-white) !important;
+span {
+  color: var(--color-white) !important;
 }
 .on-hover {
-    color: #F2A714 !important; /* Aplica o estilo ao texto */
+  color: #f2a714 !important; /* Aplica o estilo ao texto */
 }
 </style>
