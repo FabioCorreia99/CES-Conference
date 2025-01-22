e8665c03c66051af54865186798d0bad36252f78
andresjpg2000 <andresjpg2000@gmail.com>
2025-01-22 20:58:13 +0000

    Started buyTicketView and Ticket component

---
+++ src/components/Ticket.vue
@@ -0,0 +1,104 @@
+<template>
+    <div id="ticket">
+        <div v-if="hasLogo">
+            <img id="ticketLogo" src="@\assets\media\Logo + year.svg" alt="CES logo">
+        </div>
+
+        <div v-if="name" id="ticketName">
+            <p>{{ name }}</p>
+        </div>
+
+        <div v-if="occupation" id="ticketOccupation">
+            <p>{{ occupation }}</p>
+        </div>
+
+        <div class="edges">
+            <div id="topEdge" :style="{backgroundColor: primaryColor}"></div>
+            <div id="bottomEdge" :style="{backgroundColor: primaryColor}"></div>
+        </div>
+
+        <div id="QRCODE">
+            <img src="@\assets\media\QR CODE.svg" alt="QRCODE">
+        </div>
+
+        <div id="tag" :style="{backgroundColor: secondaryColor}">
+            <p id="tagText">VISITOR</p>
+        </div>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Ticket",
+        props: {
+            primaryColor: {type: String, default: "#26466D"},
+            secondaryColor: {type: String, default: "#F2A714"},
+            hasLogo: {type: Boolean, default: true},
+            name: {type: String, default: "nome"},
+            occupation: {type: String, default: "trabalho"},
+            hasAvatar: {type: Boolean, default: false},
+        }
+    }
+</script>
+
+<style scoped>
+#ticket {
+    width: 236px;
+    height: 409px;
+    background-color: white;
+    position: relative;
+    display: flex;
+    justify-content: center;
+}
+#ticketLogo {
+    transform: rotateZ(90deg);
+    transform-origin: center;
+}
+#ticketName {
+    position: absolute;
+    top: 20%;
+    color: #26466D;
+}
+#ticketOccupation {
+    position: absolute;
+    top: 25%;
+    color: #26466D;
+}
+.edges {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+}
+#topEdge {
+    width: 24px;
+    height: 100%;
+    position: absolute;
+    left: 0;
+}
+#bottomEdge {
+    position: absolute;
+    width: 24px;
+    height: 100%;
+    right: 0;
+}
+#QRCODE {
+    position: absolute;
+    top: 40%;
+    left: 20%;
+    transform: rotate(90deg);
+}
+#tag {
+    position: absolute;
+    width: calc(100% - 48px);
+    height: 44px;
+    top: 80%;
+}
+#tagText {
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%,-50%);
+    color: white;
+}
+</style>
--- src/views/BuyTicketView.vue
+++ src/views/BuyTicketView.vue
@@ -7,34 +7,67 @@
 
     <v-main>
         
-        <v-container>
-            <v-row justify="center">
+        <v-container id="main">
+            <v-row>
                 <!-- Header -->
                 <v-col cols="12" class="mt-12">
                     
-                    <h1 class="text-h4 mb-6">Buy tickets</h1>
+                    <h1>Payment Details</h1>
+                    <h4 class="mb-2">Complete your purchase by providing your payment details</h4>
 
                 </v-col>
             </v-row>
 
-            <v-row justify="center">
-                <v-col cols="6">
-                    <!-- User info -->
-                    <v-form ref="buyTicketForm" v-model="isValid" @submit.prevent="">
+            <v-row id="firstRow">
+                <v-col cols="12" md="4" class="formContainer"> 
+                    <!-- User Info Form -->
+                    <v-form ref="buyTicketForm" @submit.prevent="">
                         <!-- First & Last Name -->
                         <v-text-field 
-                        v-model="title"
+                        v-model="name"
                         label="First and last name" 
                         underlined 
                         dense 
                         required>
                         </v-text-field>
                         
+                        <v-text-field 
+                        v-model="email"
+                        label="Email" 
+                        underlined 
+                        dense 
+                        required>
+                        </v-text-field>
                         
+                        <v-text-field 
+                        v-model="occupation"
+                        label="Occupation" 
+                        underlined 
+                        dense 
+                        >
+                        </v-text-field>
 
+                        <div>Which days do you wish to attend? {{ checkedDates[0]}} <span v-if="checkedDates[1]">/</span> {{ checkedDates[1]}} <span v-if="checkedDates[2]">/</span> {{ checkedDates[2]}}</div>
+
+                        <input type="checkbox" id="7" value="07-01-25" v-model="checkedDates" class="mr-1"/>
+                        <label for="7" class="mr-2">07-01-25</label>
+
+                        <input type="checkbox" id="8" value="08-01-25" v-model="checkedDates" class="mr-1"/>
+                        <label for="8" class="mr-2">08-01-25</label>
+
+                        <input type="checkbox" id="9" value="09-01-25" v-model="checkedDates" class="mr-1"/>
+                        <label for="9" class="mr-2">09-01-25</label>
 
                     </v-form>
                 </v-col>
+
+                <v-col cols="12" md="8" class="formContainer"> 
+                    <h2>Customize your ticket</h2>
+                    <h4 class="mb-2">Your ticket, your choice</h4>
+
+                    <Ticket />
+                </v-col>
+
             </v-row>
 
         </v-container>
@@ -46,13 +79,42 @@
 
 <script>
 import Navbar from '@/components/Navbar.vue';
+import Ticket from '@/components/Ticket.vue';
+import TicketCustomizationPanel from '@/components/TicketCustomizationPanel.vue';
 
 export default {
     name: "BuyTicketView",
     components: {
         Navbar,
-    }
+        Ticket,
+        TicketCustomizationPanel,
+    },
+    data() {
+        return {
+            checkedDates: [],
+            name: "",
+            email: "",
+            occupation: "",
+        }
+    },
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.formContainer{
+    border: 1px solid #26466D;
+    border-radius: 10px;
+}
+
+#firstRow {
+    gap: 1rem;
+}
+
+@media only screen and (min-width: 961px) {
+    #firstRow {
+        flex-wrap: nowrap;
+        gap: 1rem;
+    }
+}
+
+</style>
