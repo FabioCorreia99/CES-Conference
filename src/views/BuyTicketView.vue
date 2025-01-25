45b51f36388ce8cd0e59f17a4b1ed5af4fb9ffda
andresjpg2000 <andresjpg2000@gmail.com>
2025-01-25 14:17:50 +0000

    Buy ticket responsive

--- src/views/BuyTicketView.vue
+++ src/views/BuyTicketView.vue
@@ -7,10 +7,10 @@
 
     <v-main>
         
-        <v-container id="main">
+        <div class="mainWrapper">
             <v-row>
                 <!-- Header -->
-                <v-col cols="12" class="mt-12">
+                <v-col cols="9" class="mt-12">
                     
                     <h1 class="blueText">Payment Details</h1>
                     <h4 class="mb-2 blueText">Complete your purchase by providing your payment details</h4>
@@ -18,8 +18,8 @@
                 </v-col>
             </v-row>
 
-            <v-row id="firstRow">
-                <v-col cols="12" md="3" class="formContainer"> 
+            <v-row id="firstRow" cols="12" md="3">
+                <v-col class="formContainer"> 
                     <!-- User Info Form -->
                     <v-form ref="buyTicketForm" @submit.prevent="">
                         <!-- First & Last Name -->
@@ -88,9 +88,9 @@
 
             </v-row>
 
-            <v-row id="secondRow">
+            <v-row id="secondRow" cols="12" md="3">
                 
-                <v-col cols="12" md="3" class="formContainer"> 
+                <v-col class="formContainer"> 
                     
                     <h1 class="blueText">Payment method</h1>
                     <h4 class="mb-2 blueText">Select your preferred payment method and pay securely</h4>
@@ -104,39 +104,69 @@
                             <img src="@\assets\media\Google Pay.svg" alt="Google Pay">
                         </div>
 
-                        <div class="PromotionCode">
-                            <h2>Promo Code</h2>
-                        </div>
                     </div>
 
                 </v-col>
 
                 <v-col cols="12" md="3" class="formContainer"> 
                     
-                    <h1 class="blueText">Total</h1>
-                    <hr class="blueText">
+                  
+                    <div class="totalGroup">
+                        <div class="totalWrapper">
+                            <h1 class="blueText">Total</h1>
+                            <span id="value">{{ total }} $</span>
+                        </div>
 
-                    <h3 class="blueText">Day(s)</h3>
-                    <h3 class="blueText">Discount</h3>
+                        <hr class="blueText">
+                    </div>
 
+                    <div class="daysDiscountWrapper">
+                        <div class="totalWrapper">
+                            <h3 class="blueText">Day(s)</h3>
+                            <span id="value">{{ checkedDates.length }}</span>
+                        </div>
+                        
+                        <div class="totalWrapper">
+                            <h3 class="blueText">Discount</h3>
+                            <span id="value">{{ (days * 600 * discount[days]) || 0  }} $</span>
+                        </div>
+                    </div>
+                
                 </v-col>
 
-                <v-col cols="12" md="3" class="formContainer"> 
+                <v-col cols="12" md="3" class="formContainer" id="payContainer"> 
                     
-                    
+                    <div class="termsWrapper">
+                        <input type="checkbox" class="mr-1"/>
+                        <h5 class="blueText">I have read and agree to the <b>Terms and Conditions</b> and <b>Privacy Policy</b>.</h5>
+                    </div>
+
+                    <div class="termsWrapper">
+                        <input type="checkbox" class="mr-1"/>
+                        <h5 class="blueText">I <b>consent</b> to receive <b>marketing emails</b>and <b>promotional offers</b> from <b>CTA/CES</b>.</h5>
+                    </div>
+
+                    <div class="payButton">
+                        <span id="payText">/End purchase</span>
+
+                        <span id="payText2">/Confirm and pay {{ total }} $</span>
+                       
+                    </div>
 
                 </v-col>
 
             </v-row>
-
-        </v-container>
+        </div>
 
         <router-view />
     </v-main>
+
+    <Footer />
 </v-app>
 </template>
 
 <script>
+import Footer from '@/components/Footer.vue';
 import Navbar from '@/components/Navbar.vue';
 import Ticket from '@/components/Ticket.vue';
 import TicketCustomizationPanel from '@/components/TicketCustomizationPanel.vue';
@@ -148,6 +178,7 @@ export default {
         Navbar,
         Ticket,
         TicketCustomizationPanel,
+        Footer,
     },
     data() {
         return {
@@ -156,12 +187,21 @@ export default {
             email: "",
             occupation: "",
             usersStore: useUsersStore(),
+            total: 0,
+            discount: {
+                1: 0,
+                2: 1/6,
+                3: 1/3,
+            },
         }
     },
     computed: {
         user() {
             return this.usersStore.getUserById(this.usersStore.currentUserId);
-        }
+        },
+        days() {
+            return this.checkedDates.length;
+        },
     },
     created() {
         
@@ -171,11 +211,32 @@ export default {
         }
         
     },
+    methods: {
+        calculateTotal() {
+            const dayPrice = 600;
+            const discount = this.discount[this.days] || 0;
+
+            this.total = Math.round(this.days * dayPrice * ( 1- discount));
+        }
+    },
+    watch: {
+        days(newVal) {
+            this.calculateTotal();
+        }
+    },
     
 };
 </script>
 
 <style scoped>
+
+.mainWrapper {
+    position: relative;
+    max-width: 1200px; 
+    margin: 0 auto; 
+    padding: 2rem; 
+}
+
 .formContainer{
     border: 1px solid #26466D;
     border-radius: 10px;
@@ -191,7 +252,7 @@ export default {
 
 #secondRow {
     margin-top: 2rem;
-    gap: 1rem;
+    gap: 0.5rem;
 }
 
 .customTicketContainer {
@@ -213,12 +274,11 @@ export default {
 .TicketComponent {
     grid-area: 1 / 2 / 6 / 4;
     margin-top: -4rem;
-    min-width: 153.4px;
-    min-height: 265.85px;
-    max-width: 153.4px;
+    width: 100%; 
+    max-width: 153.4px; 
+    height: auto; 
     max-height: 265.85px;
-    justify-self: center;
-
+    justify-self: center; 
 }
 
 .methodsWrapper {
@@ -231,6 +291,17 @@ export default {
     height: 36px;
     cursor: pointer;
 }
+
+.paymentMethod:hover {
+    border-radius: 10px;
+    border: 1px solid #26466D;
+}
+
+.paymentMethod:active {
+    border-radius: 10px;
+    border: 1px solid #26466D;
+}
+
 .PromotionCode {
     display: flex;
     justify-content: center;
@@ -241,12 +312,110 @@ export default {
     border-radius: 10px;
 }
 
+.totalWrapper {
+    display: flex;
+    align-items: baseline;
+    justify-content: space-between;
+}
+
+.daysDiscountWrapper {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-evenly;
+    height: 75%;
+}
+
+.termsWrapper {
+    display: flex;
+    align-items: center;
+}
+
+#payContainer {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-evenly;
+}
+
+.payButton {
+    border-radius: 10px;
+    border: 1px solid #26466D;
+    background-color: #F2A714;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding-top: 16px;
+    padding-bottom: 16px;
+    position: relative;
+    overflow: hidden; 
+    transition: background-color 0.3s ease;
+    cursor: pointer;
+}
+
+.payButton:hover {
+    background-color: white;
+}
+
+.payButton::before {
+    content: '';
+    position: absolute;
+    bottom: -80%;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: white;
+    z-index: 0;
+    transition: bottom 0.3s ease;
+}
+
+.payButton:hover::before {
+    bottom: 0;
+}
+
+#payText, #payText2 {
+    position: absolute;
+    z-index: 1;
+    transition: opacity 0.3s ease;
+    white-space: nowrap;
+}
+
+#payText {
+    opacity: 1;
+}
+
+#payText2 {
+    opacity: 0;
+    color: #26466D;
+}
+
+.payButton:hover #payText {
+    opacity: 0;
+}
+
+.payButton:hover #payText2 {
+    opacity: 1;
+}
+
 @media only screen and (min-width: 961px) {
     #firstRow {
         flex-wrap: nowrap;
         gap: 1rem;
     }
     
+}
+
+@media (max-width: 500px) {
+    .customTicketContainer {
+        grid-template-columns: 1fr; 
+        grid-template-rows: auto; 
+    }
+
+    .TicketComponent {
+        grid-area: unset;
+        transform-origin: top left;
+        /* margin-top: 1rem;  */
+        transform: scale(0.75);
+        margin: auto;  
+    }
 }
 
 </style>
